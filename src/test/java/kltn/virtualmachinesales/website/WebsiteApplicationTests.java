d75cda657962daf2b645d5bfb09176509a165442
hieumai943 <hieunm.hrt@gmail.com>
2024-08-19 23:50:08 +0700

    java hello world

---
+++ src/test/java/kltn/virtualmachinesales/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package kltn.virtualmachinesales.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
