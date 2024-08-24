8fa29977300085f2d424da4f7d5aa8fb8ec01403
hieumai943 <hieunm.hrt@gmail.com>
2024-08-24 22:02:12 +0700

    add connect to mysql

--- src/main/java/kltn/virtualmachinesales/website/repository/UserRepository.java
+++ src/main/java/kltn/virtualmachinesales/website/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package kltn.virtualmachinesales.website.repository;
 
 import kltn.virtualmachinesales.website.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByEmail(String email);
 
