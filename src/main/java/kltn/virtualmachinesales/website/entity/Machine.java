f4ac59269d7c2d3fd2c2142cc39f2724d66df776
hieumai943 <hieunm.hrt@gmail.com>
2024-09-12 11:50:21 +0700

    done API to create a payment

--- src/main/java/kltn/virtualmachinesales/website/entity/Machine.java
+++ src/main/java/kltn/virtualmachinesales/website/entity/Machine.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class Machine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+     private long id;
     private String name;
     private String description;
     private String ram;
